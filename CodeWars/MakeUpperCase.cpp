3712baa96dbabceea7d1042ce1a639de2a025263
MichaelBelsky <71600362+MichaelBelsky@users.noreply.github.com>
2022-03-05 19:52:30 +0100

    MakeUpperCase

--- CodeWars/MakeUpperCase.cpp
+++ CodeWars/MakeUpperCase.cpp
@@ -5,7 +5,15 @@ std::string makeUpperCase(const std::string& input_str)
 	string res;
 	for (auto i : input_str)
 	{
-		res += (char)toupper(i);
+		res +=toupper(i);
 	}
 	return res;
 }
+
+			//	CODEWARS
+//{
+//	std::string str = input_str;
+//	std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+//	return str;
+//}
+
